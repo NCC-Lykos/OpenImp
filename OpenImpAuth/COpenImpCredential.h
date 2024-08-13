e5ad500b476d7cabc7a4632ea7b662be8794108e
Noah Dolph <lykos@neocron.org>
2024-08-12 20:09:47 -0500

    Removed MS Sample stuff
    Removed MS Stuff about samples and yadda yadda. We know its a sample, I'm dumb.

--- OpenImpAuth/COpenImpCredential.h
+++ OpenImpAuth/COpenImpCredential.h
@@ -1,11 +1,3 @@
-//
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//
 // COpenImpCredential is our implementation of ICredentialProviderCredential.
 // ICredentialProviderCredential is what LogonUI uses to let a credential
 // provider specify what a user tile looks like and then tell it what the
--- OpenImpAuth/COpenImpProvider.cpp
+++ OpenImpAuth/COpenImpProvider.cpp
@@ -1,11 +1,3 @@
-//
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//
 // COpenImpProvider implements ICredentialProvider, which is the main
 // interface that logonUI uses to decide which tiles to display.
 // In this sample, we will display one tile that uses each of the nine
